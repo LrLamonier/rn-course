e021221e431a963a5277b780efb369ab29c81948
Lucas Lamonier <lucasrlamonier@gmail.com>
2022-07-05 16:35:38 -0300

    Finish section 05

--- 03/components/ui/Title.js
+++ 03/components/ui/Title.js
@@ -1,22 +1,23 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, Platform } from "react-native";
 
 function Title({ children }) {
-  return <Text style={styles.title}>{children}</Text>;
+	return <Text style={styles.title}>{children}</Text>;
 }
 
 export default Title;
 
 const styles = StyleSheet.create({
-  title: {
-    fontFamily: 'open-sans-bold',
-    fontSize: 24,
-    // fontWeight: 'bold',
-    color: 'white',
-    textAlign: 'center',
-    borderWidth: 2,
-    borderColor: 'white',
-    padding: 12,
-    maxWidth: '80%',
-    width: 300
-  },
+	title: {
+		fontFamily: "open-sans-bold",
+		fontSize: 24,
+		// fontWeight: 'bold',
+		color: "white",
+		textAlign: "center",
+		// borderWidth: Platform.OS === "android" ? 2 : 0,
+		borderWidth: Platform.select({ ios: 0, android: 2 }),
+		borderColor: "white",
+		padding: 12,
+		maxWidth: "80%",
+		// width: 300,
+	},
 });
