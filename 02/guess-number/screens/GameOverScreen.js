a13695a82bcdca5653a3952483777a9e2263ab31
Lucas Lamonier <lucasrlamonier@gmail.com>
2022-07-05 14:54:20 -0300

    Final commit on project 02

--- 02/guess-number/screens/GameOverScreen.js
+++ 02/guess-number/screens/GameOverScreen.js
@@ -1,13 +1,43 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../util/colors";
 
 const GameOverScreen = () => {
 	return (
 		<View>
-			<Text></Text>
+			<Title>Game over!</Title>
+			<View style={styles.imageContainer}>
+				<Image
+					style={styles.image}
+					source={require("../asses/images/gameover.png")}
+				/>
+			</View>
+			<Text>
+				Your phone needed <Text>X</Text> rounds to guess the number{" "}
+				<Text>Y</Text>!
+			</Text>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	rootContainer: {
+		flex: 1,
+		padding: 24,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	imageContainer: {
+		width: 400,
+		height: 400,
+		borderRadius: 200,
+		overflow: "hidden",
+		borderColor: Colors.primary800,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+	},
+});
 
 export default GameOverScreen;
