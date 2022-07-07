b6719abed25fdba936eb35c4f73d2c9d66697c60
Lucas Lamonier <lucasrlamonier@gmail.com>
2022-07-07 18:36:41 -0300

    Finish working on different navigators

--- 05/screens/WelcomeScreen.js
+++ 05/screens/WelcomeScreen.js
@@ -1,21 +1,10 @@
 import { View, Text, StyleSheet, StatusBar } from "react-native";
-import { useDrawerStatus } from "@react-navigation/drawer";
 import FocusAwareStatusBar from "../components/FocusAwareStatusBar";
 
 function WelcomeScreen() {
-	const isDrawerOpen = useDrawerStatus();
-
 	return (
 		<View style={styles.rootContainer}>
-			{isDrawerOpen === "open" ? (
-				<FocusAwareStatusBar
-					hidden={true}
-					translucent={true}
-					backgroundColor='transparent'
-				/>
-			) : (
-				<FocusAwareStatusBar translucent={true} backgroundColor='transparent' />
-			)}
+			<FocusAwareStatusBar translucent={true} backgroundColor='transparent' />
 			<Text>
 				This is the <Text style={styles.highlight}>"Welcome"</Text> screen!
 			</Text>
